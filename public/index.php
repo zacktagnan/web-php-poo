c8288dfe197142aa9d6d2cbeb1f4dd7e4588df02
zacktagnan <zacktagnan@gmail.com>
2023-07-11 19:54:46 +0200

    Composer-AUTOLOAD-Uso.de.Carbon.bAsico

--- public/index.php
+++ public/index.php
@@ -7,15 +7,22 @@
 </head>
 <body>
 <?php
+require '../vendor/autoload.php';
+
 use Libs\BreadCrumbs;
+use Carbon\Carbon;
 
 include '../Libs/BreadCrumbs.php';
 $crumbs = new BreadCrumbs();
 $crumbs->add('/link', 'Sección');
 $crumbs->show();
+
+$date = Carbon::now();
+$carbonYear = $date->format('Y');
 ?>
 
     <h1>Aloha!!</h1>
     <p>&copy; <?php echo date('Y'); ?></p>
+    <p>&copy; <?php echo $carbonYear . ' con CARBON'; ?></p>
 </body>
 </html>
